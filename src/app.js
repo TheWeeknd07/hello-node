e4492faba3c3f1436c392bb7d0c427c6d049374a
Aman Sharma <testfeb0202@gmail.com>
2024-11-29 17:47:33 +0530

    Dynamic Routing.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,32 @@ const express = require('express');
 const app = express();
 
 // define a route
+
+app.get('/hello', (req, res) => {
+    return res.send('hello');
+});
+
+app.get('/hello*ab', (req, res) => {
+    return res.send('hello*ab');
+});
+
+// dynamic routes
+app.get('/hello/:id', (req, res) => {
+    return res.send('dynamic routes');
+});
+
+app.get('/hello/:id', (req, res) => {
+    return res.send('dynamic routes');
+});
+
+app.get('/a+', (req, res) => {
+    return res.send('a+ routes');
+});
+
+app.get('/a*', (req, res) => {
+    return res.send('a* routes');
+});
+
 app.use('/', (req, res) => {
     res.send('Hello World!');
 });
