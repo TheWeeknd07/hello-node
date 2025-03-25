71811ced79c57673b87510fa040ac5a5c5cd984f
Aman Sharma <testfeb0202@gmail.com>
2025-03-26 00:02:28 +0530

    file2 code executed before file1 import it using require function that is why on running file1 code get stuck in infinite loop

---
+++ src/practise/file1.js
@@ -0,0 +1,3 @@
+const { printHello } = require("./file2");
+
+printHello();
---
+++ src/practise/file2.js
@@ -0,0 +1,10 @@
+while(true) {
+    console.log("while loop");
+}
+function printHello() {
+    console.log("Hello from File 2");
+}
+   
+module.exports = {
+    printHello: printHello
+}
