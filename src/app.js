6a3f7297fce4392e745d95be102d5acb9616a3cf
Aman Sharma <testfeb0202@gmail.com>
2025-04-11 01:59:43 +0530

    creating our express server: episode 03

--- src/app.js
+++ src/app.js
@@ -1,39 +1,14 @@
 const express = require('express');
 const app = express();
 
-// define a route
-
-app.get('/hello', (req, res) => {
-    return res.send('hello');
+app.use('/hello', (req, res) => {
+    return res.send('Hello hello hello!!!');
 });
 
-app.get('/hello*ab', (req, res) => {
-    return res.send('hello*ab');
+app.use((req, res) => {
+    return res.send('Hello from Server!!!');
 });
 
-// dynamic routes
-app.get('/hello/:id', (req, res) => {
-    return res.send('dynamic routes');
-});
-
-app.get('/hello/:id', (req, res) => {
-    return res.send('dynamic routes');
-});
-
-app.get('/a+', (req, res) => {
-    return res.send('a+ routes');
-});
-
-app.get('/a*', (req, res) => {
-    return res.send('a* routes');
-});
-
-app.use('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-// start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-   console.log('listening on port ' + PORT); 
+app.listen(7777, () => {
+    console.log('Server is running on port 7777');
 });
