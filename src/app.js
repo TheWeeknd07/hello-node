0fd86ab767910a475cfc6f5423cfaed40e6c7bc9
Aman Sharma <testfeb0202@gmail.com>
2025-04-13 11:48:14 +0530

    middlewares and error handling commit 3 : episode 05

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const app = express();
 
-const { adminAuth, userAuth } = require('./authenticate/auth');
+// When would if (err) be useful?
+// If you define a middleware that tries to act as both a regular and error middleware (unusual).
+// Or if you chain multiple error-handlers and want one to conditionally act based on the error value (like if err is a specific type).
+// But in most cases — especially simple ones — you can skip the if (err) entirely in your error handlers.
 
-app.use('/admin', adminAuth);
-
-app.post('/user/login', (req, res) => {
-    res.send('User Logged In Successfully');
+app.use('/', (err, req, res, next) => {
+    console.log('inside the wild card error handling at start.');
+    if(err) { // the if (err) check is not strictly required in an error-handling middleware because Express only calls it when there is an error.
+        res.status(500).send('something went wrong!')
+    }
 });
 
-app.get('/user/data', userAuth, (req, res) => {
-    res.send('User Data');
+app.get('/user', (req, res) => {
+    throw new Error('message')
+    res.send('hello from user');
 });
 
-app.get('/admin/getAllData', (req, res) =>{
-    res.send('All Data');
-});
-
-app.post('/admin/deleteUser', (req, res) => {
-    res.send('User Deleted');
-});
-
-app.use((req, res) => {
-    res.send('Default route handler or Wild card handler!!!');
+app.use('/', (err, req, res, next) => {
+    console.log('inside the wild card error handling at end.');
+    if(err) {
+        res.status(500).send('something went wrong!')
+    }
 });
 
 app.listen(7777, () => {
