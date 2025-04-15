8afdcbb00237dcd05ccafc6cb6b983e3ef0d7a76
Aman Sharma <testfeb0202@gmail.com>
2025-04-16 01:55:33 +0530

    Diving into the APIs: episode: 07

--- src/app.js
+++ src/app.js
@@ -1,30 +1,91 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-const connectDB = require('./config/database');
-const User = require('./models/user');
+const User = require("./models/user");
+
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    // Creating a new instance of the User model
-    const user = new User({
-      firstName: "Sachin",
-      lastName: "Tendulkar",
-      emailId: "sachin@kohli.com",
-      password: "sachin@123",
-    });
-  
-    try {
-      await user.save();
-      res.send("User Added successfully!");
-    } catch (err) {
-      res.status(400).send("Error saving the user:" + err.message);
-    }
-  });
+  //   Creating a new instance of the User model
+  const user = new User(req.body);
 
-connectDB().then(() => {
-    console.log('DB connected successfully');
-    app.listen(7777, () => {
-        console.log('Server is running on port 7777');
-    });
-}).catch((err) => {
-    console.error('DB connection failed!', err)
+  try {
+    await user.save();
+    res.send("User Added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
 });
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+
+    // const users = await User.find({ emailId: userEmail });
+    // if (users.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Detele a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    //const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
