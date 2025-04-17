b6caf4a0b997ba6fbe6912e2b24c66d0aa79f8a7
Aman Sharma <testfeb0202@gmail.com>
2025-04-18 00:34:42 +0530

    Data Sanitization & Schema Validations commit 2 (using validator library) : episode 8

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,6 +18,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       trim: true,
+      validate(value) {
+        if(!validator.isEmail(value)) {
+          throw new Error("Invalid email address" + value);
+        }
+      }
     },
     password: {
       type: String,
